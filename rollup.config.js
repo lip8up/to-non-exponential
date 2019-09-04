8e2c8db676f68b38a149176876509760663d5de1
张鹏 <peng.zhang@jydata.com>
2019-09-04 20:54:42 +0800

    use rollup to build

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import buble from 'rollup-plugin-buble'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      // sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      // sourcemap: true
+    }
+  ],
+  plugins: [
+    typescript(),
+    buble()
+  ]
+}
